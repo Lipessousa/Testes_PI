5b559f1b5bbe87e61795becece832dba8bb20445
Augusto Cruz <156687069+A-Cruzz@users.noreply.github.com>
2024-05-04 19:22:17 -0300

    Update
    Atualizando os pacotes de entidades

---
+++ Ambeinte_Teste_PI_Backend/src/Entidades/Usuario.java
@@ -0,0 +1,43 @@
+
+package Entidades;
+
+public class Usuario {
+    public int idUsuario;
+    public String login;
+    public String senhaUsuario;
+    public boolean administrador;
+    
+    public int getIdUsuario(){
+        return idUsuario;
+    }
+    
+    public void setIdUsuario(int idUsuario){
+        this.idUsuario = idUsuario;
+    }
+    
+    public String getLogin(){
+        return login;
+    }
+    
+    public void setLogin(String login){
+        this.login = login;
+    }
+    
+    public String getSenhaUsuario(){
+        return senhaUsuario;
+    }
+    
+    public void setSenhaUsuario(String senhaUsuario){
+        this.senhaUsuario = senhaUsuario;
+    }
+    
+    public boolean getAdministrador(){
+        return administrador;
+    }
+    
+    public void setAdministrador(boolean administrador){
+        this.administrador = administrador;
+    }
+    
+    
+}
